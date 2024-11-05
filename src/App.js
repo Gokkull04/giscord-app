5fb60b8cfe4c83da09cc10f2b8a853b7044ecc40
Gokkull04 <gokkull04@gmail.com>
2024-11-05 21:43:30 +0530

    Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ImageZoomEffect from "./ImageZoomEffect.js";
+import Home from "./pages/Home.js";
 
 function App() {
   return (
     <div className="App">
+      <Home />
       <ImageZoomEffect />
     </div>
   );
